594646a3075a8258c4069d22cbbe4a3dca0925c2
IEugeneI <eugenelboichenko@gmail.com>
2023-05-03 01:21:25 +0300

    test

--- src/Http/Helpers/VATvalidatorHelper.php
+++ src/Http/Helpers/VATvalidatorHelper.php
@@ -16,6 +16,11 @@ class VATvalidatorHelper
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
+
+      $headers = array();
+      $headers[] = "Accept: application/json";
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
       $result = curl_exec($ch);
       if (curl_errno($ch)) {
           echo 'Error:' . curl_error($ch);
