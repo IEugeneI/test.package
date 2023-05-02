e91b297cf57bad646dc12d825cf650ca545ae6e2
IEugeneI <eugenelboichenko@gmail.com>
2023-05-03 01:18:05 +0300

    test

--- src/Http/Helpers/VATvalidatorHelper.php
+++ src/Http/Helpers/VATvalidatorHelper.php
@@ -1,14 +1,41 @@
 <?php
 
 namespace Eugene\ValidateVatNumberEu\Http\Helpers;
-use Eugene\ValidateVatNumberEu\Http\Requests\ChangeProjectStatusRequest;
-use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
 class VATvalidatorHelper
 {
-  public static function validate(ChangeProjectStatusRequest $request)
+  public static function validate($vatNumber,$validateCompany=false,$validateAddress=false,$includeRawResponse=false)
   {
-        dd('123');
+      $ISO=substr($vatNumber, 0, 2);
+      $vat=substr($vatNumber,2);
+      $url='https://ec.europa.eu/taxation_customs/vies/rest-api/ms/'.$ISO.'/vat/'.$vat;
+      $ch = curl_init();
+
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
+      $result = curl_exec($ch);
+      if (curl_errno($ch)) {
+          echo 'Error:' . curl_error($ch);
+      }
+      curl_close ($ch);
+      echo $result;
+//      $validator = Validator::make([
+//          'VAT'=>$vatNumber,
+//          'companyName'=>$validateCompany,
+//          'address'=>$validateAddress,
+//          'includeRawResponse'=>$includeRawResponse
+//      ], [
+//          'VAT' => 'required|string',
+//          'companyName' => 'string',
+//          'address' => 'string',
+//          'includeRawResponse' => 'string',
+//      ]);
+//      if ($validator->fails()){
+//
+//      }
         //$vatNumber,$validateCompany=false,$validateAddress=false,$includeRawResponse=false
   }
 }
